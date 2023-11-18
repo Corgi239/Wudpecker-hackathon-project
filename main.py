91f8609ff42c447f466133a865de68836ea523f1
Tam Nguyen <tamdnguyen2111@gmail.com>
2023-11-18 16:49:03 +0200

    initial API code

--- main.py
+++ main.py
@@ -2,8 +2,24 @@
 This is the main file for the project.
 """
 
+import uvicorn
+from fastapi import FastAPI
 from src import logic
 
-if __name__ == "__main__":
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/array")
+async def array():
     test_arr = logic.example_func()
     print(test_arr)
+    return {"message": test_arr.tolist()}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="127.0.0.1")
